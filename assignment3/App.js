61d1f501623bab3b9f6dd3fb7bb96404e80aa247
ohenek01 <ohenek20@gmail.com>
2024-05-31 19:37:51 +0000

    changes

--- assignment3/App.js
+++ assignment3/App.js
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
+import React, {useState} from 'react';
+import Icon from 'react-native-vector-icons/Feather';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}> My name is <Text style={styles.boldText}>Ohene</Text></Text>
+      <Text style={styles.header}>Hello, Guys</Text>
+      <Text style={styles.headerParagraph}>14 Tasks today</Text>
+      <Image source={require('./assets/Profile Image.png')} style={styles.profile}/>
+      <View style={styles.searchArea}>
+      <TextInput style={styles.searchBar} placeholder='Search'/>
+      <Icon name='search' style={styles.icon}/>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,14 +21,47 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#6756ff',
+    backgroundColor: 'antiquewhite',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  text:{
-    fontSize: 24,
-  },
-  boldText:{
+  header: {
     fontWeight: 'bold',
-  }
+    fontSize: 24,
+    position: 'absolute',
+    top: 50,
+    left: 23,
+  },
+  headerParagraph:{
+    position:'absolute',
+    top: 80,
+    left: 22,
+  }, 
+    profile: {
+    position: 'absolute',
+    top: 55,
+    right: 30,
+    width: 30
+  },
+
+  searchArea: {
+    position: 'absolute',
+    top: 130,
+    left: 30,
+    backgroundColor: 'white',
+    width: 250,
+    height: 40,
+    borderRadius: 10
+  },
+
+  searchBar:{
+    flex: 1,
+    paddingLeft: 36,
+    fontSize: 16,
+  }, 
+  icon:{
+    fontSize: 18,
+    position: 'absolute',
+    top: 10,
+  },
 });

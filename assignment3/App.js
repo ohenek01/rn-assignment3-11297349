7fbd622539ecfa23b7d2b988c690f60559c20b8f
ohenek01 <ohenek20@gmail.com>
2024-06-01 10:40:52 +0000

    inclusion of images and creation of category section

--- assignment3/App.js
+++ assignment3/App.js
@@ -10,8 +10,19 @@ export default function App() {
       <Text style={styles.headerParagraph}>14 Tasks today</Text>
       <Image source={require('./assets/Profile Image.png')} style={styles.profile}/>
       <View style={styles.searchArea}>
-      <TextInput style={styles.searchBar} placeholder='Search'/>
-      <Icon name='search' style={styles.icon}/>
+        <TextInput style={styles.searchBar} placeholder='Search'/>
+        <Icon name='search' style={styles.icon}/>
+      </View>
+      <Text style={styles.category}>Categories</Text>
+      <View style={styles.categoryContent}>
+        <View style={styles.categoryContent1}>
+          <Text style={styles.categoryText1}>Exercise</Text>
+          <Text style={styles.categoryText2}>12 Tasks</Text>
+          <Image source={require('./assets/young woman working online.png')}/>
+        </View>
+        <View style={styles.categoryContent2}>
+          <Image source={require('./assets/young woman working at desk.png')}/>
+        </View>
       </View>
       <StatusBar style="auto" />
     </View>
@@ -49,7 +60,7 @@ const styles = StyleSheet.create({
     top: 130,
     left: 30,
     backgroundColor: 'white',
-    width: 250,
+    width: 270,
     height: 40,
     borderRadius: 10
   },
@@ -64,4 +75,42 @@ const styles = StyleSheet.create({
     position: 'absolute',
     top: 10,
   },
+  category:{
+    fontSize: 20,
+    fontWeight: 'bold',
+    position: 'absolute',
+    top: 220,
+    left: 35,
+  },
+  categoryContent:{
+    display: 'flex',
+    flexDirection: 'row',
+    minHeight: 200
+  },
+  categoryContent1:{
+    backgroundColor: 'white',
+    borderRadius: 20,
+    width: 180,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    marginRight: 15,
+  },
+  categoryContent2:{
+    backgroundColor: 'white',
+    borderRadius: 20,
+    width: 180,
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  },
+  categoryText1:{
+    fontWeight: 'bold',
+    fontSize: 15,
+    paddingRight: 80,
+    paddingBottom: 20
+  },
+  categoryText2:{
+    fontSize: 12,
+    paddingRight: 80,
+    paddingBottom: 10
+  }
 });
